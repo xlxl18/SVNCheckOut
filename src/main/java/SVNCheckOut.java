ad8a6e0428dcf7662cd1cc4e87a82da9ffeee06b
tima <tima_93@list.ru>
2018-03-16 17:49:06 +0600

    For-Pavel

--- src/main/java/SVNCheckOut.java
+++ src/main/java/SVNCheckOut.java
@@ -1,14 +1,13 @@
 import org.apache.commons.lang.ArrayUtils;
+
 import org.tmatesoft.svn.core.*;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
-import org.tmatesoft.svn.core.auth.SVNSSHAuthentication;
-import org.tmatesoft.svn.core.io.ISVNEditor;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.tmatesoft.svn.core.wc.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,97 +19,117 @@ public class SVNCheckOut {
 
     public static void main(String[] args) {
 
-        String userName = "tima_e";
-        //String userName=System.getProperty("user.name");
+        //String userName = "tima_e";
+        String userName=System.getProperty("user.name");
         String passWord = "2F10d189";
         String repoUrl = "https://192.168.4.8/svn/" + userName;
-        String folder = "/Users/tima/Documents/test";
+        //String folder = "/Users/tima/Documents/test";
+        String folder = "C:\\docs";
         File desFolder = new File(folder);
+        File svnFolder = new File(folder+"\\.svn");
+        File jarFolder =new File ("C:\\scripts");
         File[] files = desFolder.listFiles();
-        List<File> addedFiles=new ArrayList<File>();
-        List<File> modifiedFiles=new ArrayList<File>();
-        SVNProperties prop = new SVNProperties();
+        List<File> addUnverFiles=new ArrayList<File>();
+        int numElem=-1;
+        //List<File> addModifFiles=new ArrayList<File>();
         Logger logger = LoggerFactory.getLogger(SVNCheckOut.class);
-        logger.info("SVNCheckOut");
-        files = (File[]) ArrayUtils.removeElement(files, files[1]);
-
-
-        for (File list : files) {
-            System.out.println(list);
-
+        //logger.info("SVNCheckOut");
+        if (desFolder.exists() == false) {
+            desFolder.mkdir();
         }
+        for(int i=0; i<files.length;i++) {
+            //System.out.println(files[i].getPath());
+            if(files[i].getPath().contains("\\.svn")) {
+                numElem=i;
+                break;
+            }
+        }
+        if(numElem!=-1) {
+            files = (File[]) ArrayUtils.removeElement(files, files[numElem]);
+        }
+
         try {
             SVNURL url = SVNURL.parseURIEncoded(repoUrl);
             SVNRepository repo = SVNRepositoryFactory.create(url);
-            ISVNOptions options = SVNWCUtil.createDefaultOptions(desFolder,true);
+            ISVNOptions options = SVNWCUtil.createDefaultOptions(true);
+
             //write to log file about creating of repo
             logger.info("The repository has beed created");
+
             //create authenication data
             ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(userName, passWord);
             repo.setAuthenticationManager(authManager);
             logger.info("Repository ROOT: " + repo.getRepositoryRoot(true));
             logger.info("Repository UUID: " + repo.getRepositoryUUID(true));
             logger.info("Tha latest revision is: " + repo.getLatestRevision());
-            //create client manager and set authentication
-            SVNClientManager clientManager = SVNClientManager.newInstance();
-            clientManager.setAuthenticationManager(authManager);
-            logger.info("Auth manager was set");
-            //SVNCommitClient commitClient = new SVNCommitClient(authManager,null);
-            //commitClient.doCommit(files,false,"by "+userName,prop,null,false,true,SVNDepth.INFINITY);
-            //Checking out the folder
-            SVNUpdateClient updateClient = clientManager.getUpdateClient();
-            updateClient.setIgnoreExternals(false);
-            SVNRevision svnRevision = SVNRevision.create(repo.getLatestRevision());
-            //long doCheckout=updateClient.doCheckout(url, desFolder, svnRevision, svnRevision, SVNDepth.INFINITY, false);
-            //System.out.println(doCheckout);
-            logger.info("Checked out the project from repository");
 
-            //Add folder{
-            //if (args[0].equals("import")) {
-            ISVNEditor editor = repo.getCommitEditor("Test", null, true, null);
-            editor.openRoot(-1);
-            SVNStatusClient fileStatus = new SVNStatusClient(authManager,options);
-            for(File listF:files) {
-                SVNStatus svnStatus=fileStatus.doStatus(listF,false);
-                //if(svnStatus.getContentsStatus()==SVNStatusType.STATUS_UNVERSIONED)
-                if(!svnStatus.isVersioned())
-                {
-                    editor.addFile(listF.toString(), null, -1);
-                    addedFiles.add(listF);
-                    logger.info("The " + listF + " file was added");
-                }
-                else {
-                    editor.addFile(listF.toString(), null, -1);
-                    modifiedFiles.add(listF);
-                    logger.info("The modified" + listF + " file was added");
-                }
+            //create client manager and set authentication
+            SVNClientManager clientManager = SVNClientManager.newInstance(options, authManager);
+            logger.info("Auth manager was set");
+
+            //Checking out the folder
+
+
+            if (svnFolder.exists()==false) {
+                SVNUpdateClient updateClient = clientManager.getUpdateClient();
+                updateClient.setIgnoreExternals(false);
+                SVNRevision svnRevision = SVNRevision.create(repo.getLatestRevision());
+                long doCheckout = updateClient.doCheckout(url, desFolder, svnRevision, svnRevision, SVNDepth.INFINITY, false);
+                System.out.println(doCheckout);
+                logger.info("Checked out the project from repository");
             }
 
-        //}
-            //SVNWCClient svnwcClient = clientManager.getWCClient();
-            //svnwcClient.doAdd(desFolder, false, false, false, SVNDepth.INFINITY, false, false, false);
+
+            //Create svnwcclient to manage our Working Copy
+
+                SVNWCClient svnwcClient = new SVNWCClient(authManager, options);
+                SVNStatusClient fileStatus = new SVNStatusClient(authManager, options);
+                SVNCommitClient commitClient = clientManager.getCommitClient();
+                for (File listF : files) {
+                    //SVNStatus svnStatus = fileStatus.doStatus(listF, false);
+                    //if (!svnStatus.isVersioned()) {
+                     //
+                     //  addUnverFiles.add(listF);
+                     //  logger.info("The " + listF + " file was added");
+                   //} else {
+                    addUnverFiles.add(listF);
+
+                }
 
 
-        //logger.info("Updating the repository");
-            SVNCommitClient commitClient = clientManager.getCommitClient();
-            File[] filesForCom= addedFiles.toArray(new File[addedFiles.size()]);
-            File[] filesForComm=modifiedFiles.toArray(new File[modifiedFiles.size()]);
-            SVNCommitPacket commitPacket = commitClient.doCollectCommitItems(filesForCom,false,false, SVNDepth.INFINITY,null);
 
-            logger.info(commitPacket.toString());
 
-            //commitClient.doCommit(commitPacket,false,"test");
-            logger.info("Lya lya lya");
-        //logger.info("New Rebvision "+commitInfo.getNewRevision());
+                //Transform List to Arrays and add Files from Working Copy to SVN
+                File[] filesForCom = addUnverFiles.toArray(new File[addUnverFiles.size()]);
 
-    }
-            catch (SVNException e) {
+                if (filesForCom.length != 0) {
+                    svnwcClient.doAdd(filesForCom, true, false, false, SVNDepth.INFINITY, false, false, false);
+                    commitClient.doCommit(filesForCom, false, "Commited in " + new Date(), null, null, false, true, SVNDepth.INFINITY);
+                }
+
+                //if (filesForComm.length != 0) {
+                 //   svnwcClient.doAdd(filesForComm, true, false, false, SVNDepth.INFINITY, false, false, false);
+                 //   commitClient.doCommit(filesForComm, false, "Commited in " + new Date(), null, null, false, true, SVNDepth.INFINITY);
+               // }
+                //svnwcClient.doAdd(filesForComm, true, false, false, SVNDepth.INFINITY, false, false, false);
+                logger.info("Commited by " + userName);
+
+
+
+        }
+
+            catch(SVNException e){
                 logger.info("There is an error during creating and exporting repository");
                 logger.info(e.getErrorMessage().toString());
                 e.printStackTrace();
-            } catch (NullPointerException e) {
+            }
+
+            catch(NullPointerException e){
                 logger.info("Empty pointer");
-            } catch (EmptyStackException e) {
+
+            }
+
+            catch(EmptyStackException e){
                 e.printStackTrace();
             }
 
